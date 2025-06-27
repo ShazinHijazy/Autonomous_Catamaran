4f35ec8e21d461adf3edddd6abba83140d0716f8
Shazin Hijazy <shazhijazy@gmail.com>
2025-06-27 18:39:30 +0530

    updated codes

--- launch_codes/launch.py
+++ launch_codes/launch.py
@@ -4,45 +4,45 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='sensor_nodes',
+            package='catamaran_ros',
             executable='imu_node',
             name='imu_node',
             output='screen'
         ),
         Node(
-            package='sensor_nodes',
+            package='catamaran_ros',
             executable='gnss_node',
             name='gnss_node',
             output='screen'
         ),
         Node(
-            package='sensor_nodes',
+            package='catamaran_ros',
             executable='rplidar_launch',
             name='rplidar_node',
             output='screen'
         ),
         Node(
-            package='sensor_nodes',
+            package='catamaran_ros',
             executable='ultrasonic_waterproof_node',
             name='ultrasonic_waterproof_node',
             output='screen'
         ),
         Node(
-            package='sensor_nodes',
-            executable='ultrasonic_servo_node',
-            name='ultrasonic_servo_node',
+            package='catamaran_ros',
+            executable='ultrasonic_node',
+            name='ultrasonic_node',
             output='screen'
         ),
-        Node(
-            package='sensor_nodes',
-            executable='power_distribution_board_node',
-            name='power_distribution_board_node',
-            output='screen'
-        ),
-        Node(
-            package='sensor_nodes',
-            executable='voltage_and_current_node',
-            name='voltage_and_current_node',
-            output='screen'
-        ),
+       # Node(
+            #package='sensor_nodes',
+            #executable='power_distribution_board_node',
+            #name='power_distribution_board_node',
+            #output='screen'
+        #),
+        #Node(
+         #   package='catamaran_ros',
+          #  executable='voltage_and_current_node',
+           # name='voltage_and_current_node',
+            #output='screen'
+        #),
     ])
